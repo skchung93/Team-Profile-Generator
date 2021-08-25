4919e4e57afcac03a64f22a08ba0851bdc6cf1f9
skchung93 <skchung93@gmail.comgit config --global user.email>
2021-08-24 18:04:44 -0700

    Committing the requirements for the index.js (aka the classes and subclasses)

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
+const Employee = require('./lib/employee');
+const Manager = require ('./lib/manager');
+const Engineer = require('./lib/engineer');
+const Intern = require ('./lib/intern');
+
+
